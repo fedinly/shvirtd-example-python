50663d56f96beb17f1663a6259302fd7c4e2ef8d
fedinly <fedinly@gmail.com>
2026-03-22 08:55:49 +0000

    commit two

--- mysql/create.sh
+++ mysql/create.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+pw= (cat ./secrets/db_root_pw.txt)
+pwb= (cat ./secrets/db_backup_pw.txt)
 set -e
 
-mysql -u root -p$MYSQL_ROOT_PASSWORD <<EOSQL
+mysql -u root -p$pw <<EOSQL
 
-CREATE USER IF NOT EXISTS '$MYSQL_BACKUPER_USER'@'%' IDENTIFIED WITH mysql_native_password BY '$MYSQL_BACKUPER_PASSWORD';
+CREATE USER IF NOT EXISTS '$MYSQL_BACKUPER_USER'@'%' IDENTIFIED WITH mysql_native_password BY '$pwb';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_BACKUPER_USER'@'%' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
